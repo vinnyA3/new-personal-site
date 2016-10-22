4334643a06acee4bc7af549136dd3b521b5c1d5a
vinnya3 <vinaceto@yahoo.com>
2016-10-22 04:29:55 -0400

    feat: new render

--- bundle.js
+++ bundle.js
@@ -437,10 +437,16 @@
 	  var FormControl = (function() {
 	    function initialize() {
 	      var el = document.querySelector('.card__top-container');
-	      var form = el.querySelector('.card__contact-form')
-	      var closeForm = form.querySelector('.btn-cancel')
+	      var form = el.querySelector('.card__contact-form');
+	      var closeForm = form.querySelector('.btn-cancel');
 	      var activateForm = el.querySelector('.btn');
-	      activateForm.addEventListener("click", function() {
+	      var submitForm = form.querySelector('.btn-success');
+	      submitForm.addEventListener("click", function(e) {
+	        e.preventDefault();
+	        alert("I didn't code this yet! Sorry, please refer to my social medias!");
+	      });
+	      activateForm.addEventListener("click", function(e) {
+	        e.preventDefault();
 	        this.style.display = "none";
 	        form.style.position = "static";
 	        form.style.transform = "translateY(0em)";
