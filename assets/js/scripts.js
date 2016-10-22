183b14c61d09b88b1f6a960538fcd81e19dfbb93
vinnya3 <vinaceto@yahoo.com>
2016-10-22 03:37:11 -0400

    feat: add form control module - will refactor
    Added form control module that controls when the form should display/not.  This module will be responsible for submitting the form to the server.  Also, I will refactor this into its own module.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,47 @@
 (function() {
-
-  var drawerHero = document.querySelector('.drawer__title');
-	var date = new Date();
-	var hour = date.getHours();
-	console.log(hour)
-
-	switch (hour) {
-		case 4,5,6:
-			drawerHero.innerHTML = 'You\'re up early, don\'t forget to grab a coffee!';
-			break;
-		case 7,8,9,10,11:
-			drawerHero.innerHTML = 'Good Morning, welcome to my site!';
-			break;
-		case 12,13,14,15,16,17,18,19,20,21,22,23:
-			drawerHero.innerHTML = 'Good Evening, I hope you enjoy your stay!';
-			break;
-		case 24,0,1,2,3:
-			drawerHero.innerHTML = 'You\'re up late!  Enjoy my site you nightowl you!';
-			break;
-		default:
-			break;
-	}
-
+  var MessageControl = (function() {
+    var el = document.querySelector('.card__top-container');
+    var hero = el.querySelector('.card__hero-text');
+    var drawerHero = hero.querySelector('p');
+    var date = new Date();
+    var hour = date.getHours();
+    
+    switch (hour) {
+      case 4, 5, 6:
+        drawerHero.innerHTML = 'You\'re up early, don\'t forget to grab a coffee!';
+        break;
+      case 7, 8, 9, 10, 11:
+        drawerHero.innerHTML = 'Good Morning, welcome to my site!';
+        break;
+      case 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23:
+        drawerHero.innerHTML = 'Good Evening, I hope you enjoy your stay!';
+        break;
+      case 24, 0, 1, 2, 3:
+        drawerHero.innerHTML = 'You\'re up late!  Enjoy my site you nightowl you!';
+        break;
+      default:
+        break;
+    }
+  })();
+  
+  var FormControl = (function() {
+    function initialize() {
+      var el = document.querySelector('.card__top-container');
+      var form = el.querySelector('.card__contact-form')
+      var closeForm = form.querySelector('.btn-cancel')
+      var activateForm = el.querySelector('.btn');
+      activateForm.addEventListener("click", function() {
+        this.style.display = "none";
+        form.style.position = "static";
+        form.style.transform = "translateY(0em)";
+      });
+      closeForm.addEventListener("click", function() {
+        form.style.transform = "translateY(100em)";
+      });
+    }
+    return {
+      init: initialize
+    }
+  })();
+  FormControl.init();
 })();
