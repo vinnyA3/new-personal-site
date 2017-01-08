413bb37070a656fc481c37e17b589bc083acdf5b
vinnya3 <vinaceto@yahoo.com>
2017-01-08 14:44:35 -0500

    feat: add new js import

--- site_remodel/assets/js/main.js
+++ site_remodel/assets/js/main.js
@@ -1,6 +1,9 @@
 // SASS Include
 require('../../sass/_main.scss');
+
 // Blog APP
 require('./blog-window');
 // main-header-navi
 require('./main-header-navi');
+// Animate Site Content
+require('./animate-site-content');
